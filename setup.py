32a27e9d3b932c75efca15791ae82f7315a92ffd
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-07-04 13:50:12 +0100

    Continue working on README files.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="smg-meshing",
@@ -9,7 +9,7 @@ setup(
     author="Stuart Golodetz",
     author_email="stuart.golodetz@cs.ox.ac.uk",
     description="Mesh-related functionality",
-    long_description="",  #long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sgolodetz/smg-meshing",
     packages=find_packages(include=["smg.meshing", "smg.meshing.*"]),
